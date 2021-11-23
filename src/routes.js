811476b5996c8a69dbd0c558a2bf967a23bc6ddb
SnzDev <tms.working@gmail.com>
2021-11-23 01:25:10 -0300

    feat: Add routes✨

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,31 @@ const routes = require('express').Router();
 routes.get('/', async (req, res) => {
     res.json('msg: Hello World!')
 })
-routes.post('/api/v1/clientes', )
+
+
+routes.post('/api/v1/clientes')//CREATE
+routes.get('/api/v1/clientes')//READ
+routes.put('/api/v1/cliente/:uuid')//EDIT
+routes.get('/api/v1/cliente/:uuid')//SHOW
+routes.delete('/api/v1/cliente/:uuid')//SOFT-DELETE
+
+routes.post('/api/v1/enderecos')//CREATE
+routes.get('/api/v1/enderecos')//READ
+routes.put('/api/v1/endereco/:uuid')//EDIT
+routes.get('/api/v1/endereco/:uuid')//SHOW
+routes.delete('/api/v1/endereco/:uuid')//SOFT-DELETE
+
+routes.post('/api/v1/pontos')//CREATE
+routes.get('/api/v1/pontos')//READ
+routes.delete('/api/v1/ponto/:uuid')//SOFT-DELETE
+
+routes.post('/api/v1/contratos')//CREATE
+routes.get('/api/v1/contratos')//READ
+routes.get('/api/v1/endereco/:uuid')//SHOW
+routes.delete('/api/v1/endereco/:uuid')//SOFT-DELETE
+
+routes.get('/api/v1/endereco/:uuid/historico')//SHOW
+
+
 
 module.exports = routes;
